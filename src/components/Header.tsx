7accff008431f7f993e5ebc86746c8a96cd231ba
Crazy_Pokeking <tsaakashkavin@gmail.com>
2025-09-06 10:07:15 +0530

    hell no

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
             <Lightbulb className="w-5 h-5 text-white" />
           </div>
           <span className="font-bold text-xl bg-gradient-primary bg-clip-text text-transparent">
-            IdeaHub
+            NO IDEA
           </span>
         </div>
         
